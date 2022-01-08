62b80b9defa628dac8a6cfe3a549de502abca3c2
ToF- <cthibaut@octo.com>
2022-01-08 11:17:59 -0500

    white sqaure atop diagram

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,13 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
 sq :: Diagram B 
-sq = square 1
+sq = square 1 # fc black
+
+clearSquare :: Diagram B
+clearSquare = square 1 # fc white
 
 sq4x4 :: Diagram B
 sq4x4 = vcat (replicate 4 line)
     where line = hcat (replicate 4 sq)
 
-main = mainWith sq4x4
+main = mainWith $ clearSquare `atop` sq4x4 
