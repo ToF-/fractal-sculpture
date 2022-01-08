634e0c3bd35325140f6209c45ac311f043b7d6d8
ToF- <cthibaut@octo.com>
2022-01-08 12:39:31 -0500

    carve and reproduce

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,19 @@ import Diagrams.Backend.SVG.CmdLine
 sq :: Diagram B 
 sq = square 1 # fc black
 
-clearSquare :: Diagram B
-clearSquare = square 1 # fc white
+cl :: Diagram B
+cl = square 1 # fc white
 
-sq4x4 :: Diagram B
-sq4x4 = vcat (replicate 4 line)
-    where line = hcat (replicate 4 sq)
+cls :: Diagram B
+cls = translate (r2 (-1.5,1.5)) $ foldl (\d v -> d `atop` translate (r2 v) cl) mempty [(1.0,0.0),(2,0),(0.0,-1.0),(1.0,-3.0),(2.0,-3.0)]
 
-main = mainWith $ clearSquare `atop` sq4x4 
+carve :: Diagram B -> Diagram B
+carve d = cls `atop` (d # scale 4)
+
+reproduce :: Diagram B -> Diagram B
+reproduce d = vcat (replicate 2 (hcat (replicate 2 d)))
+
+shrink :: Diagram B -> Diagram B
+shrink = scale 0.5
+
+main = mainWith $ reproduce $ carve $ sq 
